efc8243f50adb06a3e1209171db1832f73f1351c
Cem KIRAY <jamcry@hotmail.com>
2019-12-27 11:10:28 +0300

    Implement grid layout for Results page

--- src/components/GridGallery.js
+++ src/components/GridGallery.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
+import "./GridGallery.css";
 
 export default class GridGallery extends Component {
   render() {
+    // Create random size images for development
+    const images = [];
+    for(let i=1; i<15; i++) {
+      const width = Math.random() > 0.5 ? "300":"600";
+      const height = Math.random() > 0.5 ? "200":"400";
+      const imgSrc = "https://picsum.photos/"+width+"/"+height+"?random=" + i;
+      const img = <img src={imgSrc} alt="random" />
+      images.push(img);
+    }
+
     return (
       <div className="results-grid">
-        <h2>Image Grid Here</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia quasi. Error voluptatum repellendus qui possimus eaque ex. Quasi labore animi voluptas repudiandae repellendus sequi ad aut facilis officiis possimus?</p>
+        <h2>Search Results</h2>
+        <div className="gallery-grid">
+          {images}
+        </div>
       </div>
     )
   }
