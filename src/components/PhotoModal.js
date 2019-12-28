1dcb9683ac7d911c490a8f771495d0fc5524fe58
Cem KIRAY <jamcry@hotmail.com>
2019-12-28 18:13:47 +0300

    Add click handler for PhotoModal (fixes #1)

--- src/components/PhotoModal.js
+++ src/components/PhotoModal.js
@@ -2,8 +2,15 @@ import React from 'react'
 import "./PhotoModal.css";
 
 const PhotoModal = (props) => {
+  const handleClick = e => {
+    // Verify that the outside of the content is clicked
+    if(e.target.className==="modal-container") {
+      props.closeModal()
+    }
+  }
+  
   return (
-    <div className="modal-container" onClick={props.closeModal}>
+    <div className="modal-container" onClick={handleClick}>
       <div className="content">
         <h1>MODAL CONTENT</h1>
         <img src={props.data.url} alt="expanded" />
