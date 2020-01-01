e57888e67291068c51eef31d9a01f332f52e5d01
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-01 15:58:05 +0000

    Deploy jamcry/unsplash-client-hipo to github.com/jamcry/unsplash-client-hipo.git:gh-pages

---
+++ precache-manifest.b9be0ef16bdcebc893354a0d0272c3c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cac0994e9efc6448bcd801d5859a370",
+    "url": "/unsplash-client-hipo/index.html"
+  },
+  {
+    "revision": "9bba44ef140b8189b074",
+    "url": "/unsplash-client-hipo/static/css/main.ccd59d55.chunk.css"
+  },
+  {
+    "revision": "c17614f729d91be90ffa",
+    "url": "/unsplash-client-hipo/static/js/2.c0742484.chunk.js"
+  },
+  {
+    "revision": "dfeea506dedb1f8ad64e8d3829765f8f",
+    "url": "/unsplash-client-hipo/static/js/2.c0742484.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bba44ef140b8189b074",
+    "url": "/unsplash-client-hipo/static/js/main.7d464baa.chunk.js"
+  },
+  {
+    "revision": "148c8cc975fa4c02eeef",
+    "url": "/unsplash-client-hipo/static/js/runtime-main.5371a7ed.js"
+  },
+  {
+    "revision": "ae74bff3954e21a8c2c207ac53ab9a66",
+    "url": "/unsplash-client-hipo/static/media/logo.ae74bff3.svg"
+  }
+]);
